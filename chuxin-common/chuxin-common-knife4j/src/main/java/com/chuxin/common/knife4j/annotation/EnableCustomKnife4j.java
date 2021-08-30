e168113e0db8ba9397c8ae8ae79422173b449f77
chuxin <87849092@qq.com>
2021-08-30 21:14:09 +0800

    feat ： 新增api knife4j子模块

---
+++ chuxin-common/chuxin-common-knife4j/src/main/java/com/chuxin/common/knife4j/annotation/EnableCustomKnife4j.java
@@ -0,0 +1,18 @@
+package com.chuxin.common.knife4j.annotation;
+
+import com.chuxin.common.knife4j.config.Knife4jConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 初心
+ * @date 2021/8/30 21:05
+ */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({ Knife4jConfiguration.class })
+public @interface EnableCustomKnife4j {
+}
