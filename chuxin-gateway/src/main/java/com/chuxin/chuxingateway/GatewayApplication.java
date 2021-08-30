bdd009a61f8b2fdd2c5ea9d3bf13d955d890ecc6
chuxin <87849092@qq.com>
2021-08-30 21:18:19 +0800

    chore ： 启动方式增加自定义注解

--- chuxin-gateway/src/main/java/com/chuxin/chuxingateway/GatewayApplication.java
+++ chuxin-gateway/src/main/java/com/chuxin/chuxingateway/GatewayApplication.java
@@ -1,12 +1,16 @@
 package com.chuxin.chuxingateway;
 
+import com.chuxin.common.knife4j.annotation.EnableCustomKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 初心
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCustomKnife4j
 public class GatewayApplication {
 
     public static void main(String[] args) {
